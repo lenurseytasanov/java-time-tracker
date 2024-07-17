4640ec111143d40dc3e8f7e45301d9e6a2e54387
lenurseytasanov <lenur.seytasanov@mail.ru>
2024-07-17 19:03:12 +0500

    init commit

---
+++ src/main/java/edu/spring/javatimetracker/JavaTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.javatimetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/spring/javatimetracker/JavaTimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spring.javatimetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
